c0aea04db428928747539630707aee398548deb8
Tim Davies <tim.davies@opendataservices.coop>
2017-07-04 14:12:40 +0200

    Updated draft with schema

--- docs/conf.py
+++ docs/conf.py
@@ -134,6 +134,9 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_context = {
+        'css_files':['_static/theme_overrides.css']
+    }
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Theme options are theme-specific and customize the look and feel of a theme

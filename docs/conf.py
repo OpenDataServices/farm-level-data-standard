7d37153073510c8cb89e7b06deebdb5c75c98eaf
Tim Davies <tim.davies@opendataservices.coop>
2017-07-10 16:30:26 +0100

    Adding merge process

--- docs/conf.py
+++ docs/conf.py
@@ -368,3 +368,26 @@ def setup(app):
         'enable_eval_rst': True
         }, True)
     app.add_transform(AutoStructify)
+
+    import json
+    import json_merge_patch
+    import os
+    import glob
+
+
+    schema = {}
+
+    for file in glob.glob("../schema/components/*.JSON"):
+        try:
+            with open(file,'r') as schema_file:
+                print("Merging "+ file)
+                schema_element = json.loads(schema_file.read())
+                schema = json_merge_patch.merge(schema, schema_element)
+        except Exception:
+            print("Problem merging from " + file)
+            pass
+
+    with open("../schema/first-mile-schema.json","w") as outfile:
+        outfile.write(json.dumps(schema,indent=2))
+
+    print("Full schema in first-mile-schema.json updated")

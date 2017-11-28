04e26a306cecdac49e4ca082406fc065fb305bde
andrejellema <andrejellema@gmail.com>
2017-11-28 17:30:12 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -390,7 +390,7 @@ def setup(app):
             with open(file,'r') as schema_file:
                 print("Merging "+ file)
                 schema_element = json.loads(schema_file.read(), object_pairs_hook=OrderedDict)
-                schema = json_merge_patch.merge(schema, schema_element, position='first')
+                schema = json_merge_patch.merge(schema, schema_element, position='last')
         except Exception:
             print("Problem merging from " + file)
             pass

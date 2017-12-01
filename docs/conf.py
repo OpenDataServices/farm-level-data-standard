ee52a917a97e744af813e1283066721cbf19e6ba
andrejellema <andrejellema@gmail.com>
2017-12-01 12:34:17 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -402,7 +402,25 @@ def setup(app):
 
     from flattentool import create_template, unflatten, flatten
     create_template(schema="_static/first-mile-farm-data-schema.json",output_name="_static/flattened")
+    
+    for file in glob.glob("marsadoptionobservations/*.json") + glob.glob("components/*.json"):
+    try:
+        with open(file,'r') as schema_file:
+            print("Merging "+ file)
+            schema_element = json.loads(schema_file.read(),object_pairs_hook=OrderedDict)
+            schema = json_merge_patch.merge(schema, schema_element,position='last')
+    except Exception:
+        print("Problem merging from " + file)
+        pass
 
+    with open("first-mile-marsadoptionobservations.json","w") as outfile:
+    outfile.write(json.dumps(schema,indent=2))
+
+    from flattentool import create_template, unflatten, flatten
+    create_template(schema="_static/first-mile-marsadoptionobservations.json",output_name="_static/flattened")
+    
+    print("Full schema in first-mile marsadoptionobservations.json updated")
+    
     global html_static_path
     for file in glob.glob("extensions/*/*.json"):
         html_static_path = html_static_path + [file]

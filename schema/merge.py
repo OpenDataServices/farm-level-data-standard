6aca3584cdfea21c49e353f845d7cc00a95dbfc8
andrejellema <andrejellema@gmail.com>
2017-12-04 14:57:51 +0100

    Update merge.py

--- schema/merge.py
+++ schema/merge.py
@@ -41,22 +41,7 @@ with open("first-mile-farm-data-marsadoptionobservations-schema.json","w") as ou
 
 create_template(schema="first-mile-farm-data-marsadoptionobservations-schema.json",output_name="../csv",output_format='csv')
 
-schema = OrderedDict()
 
-for file in glob.glob("cocoaaction/*.json") + glob.glob("components/*.json"):
-    try:
-        with open(file,'r') as schema_file:
-            print("Merging "+ file)
-            schema_element = json.loads(schema_file.read(),object_pairs_hook=OrderedDict)
-            schema = json_merge_patch.merge(schema, schema_element,position='last')
-    except Exception:
-        print("Problem merging from " + file)
-        pass
-
-with open("first-mile-farm-data-cocoaaction-schema.json","w") as outfile:
-    outfile.write(json.dumps(schema,indent=2))
-
-create_template(schema="first-mile-farm-data-cocoaaction-schema.json",output_name="../csv",output_format='csv')
 
 
 

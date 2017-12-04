02b88eba7bd4c4b915e4318f654a96343a0c1e84
andrejellema <andrejellema@gmail.com>
2017-12-04 11:51:37 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -401,7 +401,7 @@ def setup(app):
     print("Full schema in first-mile-schema.json updated")
 
     from flattentool import create_template, unflatten, flatten
-    create_template(schema="_static/first-mile-farm-data-schema.json",output_name="_static/flattened")
+    create_template(schema="_static/first-mile-farm-data-marsadoptionobservations-schema.json",output_name="_static/flattened")
 
     global html_static_path
     for file in glob.glob("extensions/*/*.json"):

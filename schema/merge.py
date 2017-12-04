260da5eb6112a25b54e7b435ce30a0f5dd2038aa
andrejellema <andrejellema@gmail.com>
2017-12-04 11:56:17 +0100

    Update merge.py

--- schema/merge.py
+++ schema/merge.py
@@ -7,6 +7,26 @@ from flattentool import create_template, unflatten, flatten
 
 schema = OrderedDict()
 
+for file in glob.glob("firstmilefarmdataframe/*.json") + glob.glob("components/*.json"):
+    try:
+        with open(file,'r') as schema_file:
+            print("Merging "+ file)
+            schema_element = json.loads(schema_file.read(),object_pairs_hook=OrderedDict)
+            schema = json_merge_patch.merge(schema, schema_element,position='last')
+    except Exception:
+        print("Problem merging from " + file)
+        pass
+
+with open("first-mile-farm-data-schema.json","w") as outfile:
+    outfile.write(json.dumps(schema,indent=2))
+
+create_template(schema="first-mile-farm-data-schema.json",output_name="../csv",output_format='csv')
+
+
+print("Full schema in first-mile-farm-data-schema.json updated")
+
+schema = OrderedDict()
+
 for file in glob.glob("marsadoptionobservations/*.json") + glob.glob("components/*.json"):
     try:
         with open(file,'r') as schema_file:
@@ -20,7 +40,7 @@ for file in glob.glob("marsadoptionobservations/*.json") + glob.glob("components
 with open("first-mile-farm-data-marsadoptionobservations-schema.json","w") as outfile:
     outfile.write(json.dumps(schema,indent=2))
 
-create_template(schema="first-mile-farm-data-schema.json",output_name="../csv",output_format='csv')
+create_template(schema="first-mile-farm-data-marsadoptionobservations-schema.json",output_name="../csv",output_format='csv')
 
 
-print("Full schema in first-mile-farm-data-schema.json updated")
+print("Full schema in first-mile-farm-data-marsadoptionobservations-schema updated")
